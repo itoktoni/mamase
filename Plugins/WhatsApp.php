bf4fda3ffd5cb57c502c74b6f531a915a9ef083d
itoktoni <itok.toni@gmail.com>
2023-08-15 17:10:24 +0700

    update

--- Plugins/WhatsApp.php
+++ Plugins/WhatsApp.php
@@ -43,7 +43,7 @@ class WhatsApp
             $response = curl_exec($curl);
 
             curl_close($curl);
-            return $response;
+            return json_decode($response);
 
         } catch (\Throwable $th) {
             $error = [
