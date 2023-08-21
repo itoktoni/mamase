8f95127a62298f0d4f597f66634fb7beb8a5edf0
itoktoni <itok.toni@gmail.com>
2023-08-21 14:53:41 +0700

    update

--- Plugins/WhatsApp2.php
+++ Plugins/WhatsApp2.php
@@ -22,25 +22,28 @@ class WhatsApp
 
         try {
             $curl = curl_init();
-            $token = $api_key;
-            $data = [
-            'phone' => $no_hp,
-            'message' => $pesan,
-            ];
-            curl_setopt($curl, CURLOPT_HTTPHEADER,
-                array(
-                    "Authorization: $token",
-                )
-            );
-            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
-            curl_setopt($curl, CURLOPT_URL,  "https://jogja.wablas.com/api/send-message");
-            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
-            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-            $result = curl_exec($curl);
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://wa.srv1.wapanels.com/send-message',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_SSL_VERIFYHOST => 0,
+                CURLOPT_SSL_VERIFYPEER => 0,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => json_encode($data),
+                CURLOPT_HTTPHEADER => array(
+                    'Content-Type: application/json',
+                ),
+            ));
+
+            $response = curl_exec($curl);
+
             curl_close($curl);
-            return $result;
+            return json_decode($response);
 
         } catch (\Throwable $th) {
             $error = [
