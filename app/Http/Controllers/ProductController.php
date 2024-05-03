997e9659484fab11bda6f0f0e4b1ba03381b6f0c
itoktoni <itok.toni@gmail.com>
2024-05-03 20:43:07 +0700

    update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -93,7 +93,7 @@ class ProductController extends MasterController
             'item' => Product::with(['has_category', 'has_brand', 'has_location'])->find($code)
         ];
         $pdf = PDF::loadView(Template::print(SharedData::get('template'), 'print'), $data);
-        return $pdf->setPaper(array( 0 , 0 , 300 , 100 ))->stream(Uuid::uuid4()->toString().'.pdf');
+        return $pdf->setPaper(array( 0 , 0 , 156 , 113 ))->stream(Uuid::uuid4()->toString().'.pdf');
     }
 
 }
