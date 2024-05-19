94244218aad918faa6f941e0578bd44d72840877
itoktoni <itok.toni@gmail.com>
2024-05-19 07:51:32 +0700

    update

--- resources/views/pages/product/print.blade.php
+++ resources/views/pages/product/print.blade.php
@@ -35,7 +35,7 @@
 
 <body>
 	<div class="container" style="text-align: center;">
-		<h5 style="margin-top:10px;font-size:13px;margin-bottom:-5px;">{{ $item->product_name }}</h5>
+		<h5 style="margin-top:15px;font-size:13px;margin-bottom:-5px;">{{ $item->product_name }}</h5>
 		<h5 style="margin: 0px auto;text-align:center">
 			<img style="margin-top:10px;height:50px" src="data:image/png;base64,{{BARCODE2D::getBarcodePNG(strval($item->product_id), 'QRCODE')}}"
 			alt="barcode" />
