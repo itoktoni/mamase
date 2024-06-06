6dd73f69c40de46ee2af7102105ba1d314de86c9
itoktoni <itok.toni@gmail.com>
2024-06-06 12:53:18 +0700

    update

--- resources/views/pages/location/print.blade.php
+++ resources/views/pages/location/print.blade.php
@@ -37,7 +37,7 @@
 	<div class="container" style="text-align: center;">
 		<p style="font-size: 5px;margin-top:0px;margin-bottom:0px">.</p>
 		<h5 style="margin-top:5px;font-size:15px;margin-bottom:-5px;">
-			<span style="margin-left: -30px;font-size:5px">.</span> <span style="margin-left: 10px">{{ $item->location_name }}</span>
+			<span style="margin-left: -40px;font-size:5px">.</span> <span style="margin-left: 10px">{{ $item->location_name }}</span>
 		</h5>
 		<h5 style="margin: 0px auto;text-align:center">
 			<img style="margin-top:10px;height:50px;margin-left:-30px" src="data:image/png;base64,{{BARCODE2D::getBarcodePNG(strval($item->location_id), 'QRCODE')}}" alt="barcode" />
