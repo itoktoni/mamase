b3622001aa198a8e67e4ebe8e2917b4ac2d4ddce
itoktoni <itok.toni@gmail.com>
2024-07-05 14:10:06 +0700

    update

--- resources/views/pages/product/test.blade.php
+++ resources/views/pages/product/test.blade.php
@@ -1,21 +1,16 @@
-<div class="card mt-3">
-	<div class="card-body">
-		<div class="row">
-			<div class="col-md-12">
-				<h5 style="text-align: center">
-					{{ $product->product_name ?? '' }}
-				</h5>
+<div style="text-align: center">
 
-				<h5 style="margin-top: -10px;;text-align:center">
-					<img style="margin-top:0pxmargin-bottom:20px;height:50px" src="data:image/png;base64,{{BARCODE2D::getBarcodePNG(route('detail', ['code' => $product->product_id]), 'QRCODE')}}"
-					alt="barcode" />
-				</h5>
+	<h5 style="text-align: center;margin-top:10px">
+		{{ $product->product_name ?? '' }}
+	</h5>
 
-				<h5 style="text-align:center;:3px;font-size:15px;margin-top:-10px">{{ $product->product_serial_number }}</h5>
+	<h5 style="margin-top: -10px;text-align:center">
+		<img style="margin-top:0pxmargin-bottom:20px;height:50px" src="data:image/png;base64,{{BARCODE2D::getBarcodePNG(route('detail', ['code' => $product->product_id]), 'QRCODE')}}"
+		alt="barcode" />
+	</h5>
 
-			</div>
-		</div>
-	</div>
+	<h5 style="text-align:center;:3px;font-size:15px;margin-top:-10px">{{ $product->product_serial_number }}</h5>
+
 </div>
 
 <script>
