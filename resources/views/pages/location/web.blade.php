f4cf92cae5e1c196e9324a4a8f0a24da98d51774
itoktoni <itok.toni@gmail.com>
2024-07-05 14:43:08 +0700

    update mamase

--- resources/views/pages/location/web.blade.php
+++ resources/views/pages/location/web.blade.php
@@ -1,17 +1,18 @@
-<div style="text-align: center">
-	<div style="margin-top: -10px">.</div>
-	<h5 style="text-align: center;margin-top:5px">
-		{{ $item->field_name ?? '' }}
-	</h5>
+<body style="position: relative">
+	<div style="text-align: center;">
+		<div style="margin-top: -10px">.</div>
+		<h5 style="text-align: center; margin-top:5px">
+			<span style="position: absolute; left:0px;font-size:15px">.</span>
+			{{ $item->field_name ?? '' }}
+			<span style="position: absolute; right:0px;font-size:15px">.</span>
+		</h5>
 
-	<h5 style="margin-top: -15px;text-align:center">
-		<img style="margin-top:0px;margin-bottom:0px;height:60px" src="data:image/png;base64,{{BARCODE2D::getBarcodePNG(strval($item->field_primary), 'QRCODE')}}"
-		alt="barcode" />
-	</h5>
-
-	<h5 style="text-align:center;font-size:15px;margin-top:-15px">{{ $item->product_serial_number }}</h5>
-
-</div>
+		<h5 style="margin-top: -15px;text-align:center">
+			<img style="margin-top:0px;margin-bottom:0px;height:70px" src="data:image/png;base64,{{BARCODE2D::getBarcodePNG(strval($item->field_primary), 'QRCODE')}}"
+			alt="barcode" />
+		</h5>
+	</div>
+</body>
 
 <script>
 	Website2APK.printPage();
