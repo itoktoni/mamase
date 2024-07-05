dae41d031f422d9dcc6c61cd75dadbd97c67ea21
itoktoni <itok.toni@gmail.com>
2024-07-05 14:00:03 +0700

    update

--- resources/views/pages/product/test.blade.php
+++ resources/views/pages/product/test.blade.php
@@ -1,38 +1,26 @@
-@section('title')
-<h4>Data Riwayat Perawatan {{ $model->field_name ?? '' }}</h4>
-@endsection
-
-@section('container')
-
-@if(!request()->ajax())
-<div class="">
-	<div class="header-container container-fluid d-sm-flex justify-content-between">
-		@yield('title')
-	</div>
-</div>
-@endif
-
 <div class="card mt-3">
 	<div class="card-body">
 		<div class="row">
-			<div class="col-md-12 mb-3">
-				<h5 class="text-left">
+			<div class="col-md-12">
+				<h5 style="text-align: center">
 					{{ $product->product_name ?? '' }}
 				</h5>
 
-				<h5 style="margin: 0px auto;">
-					<img style="margin-top:10pxmargin-bottom:20px;height:70px" src="data:image/png;base64,{{BARCODE2D::getBarcodePNG(route('detail', ['code' => $product->product_id]), 'QRCODE')}}"
+				<h5 style="margin-top: -10px;;text-align:center">
+					<img style="margin-top:0pxmargin-bottom:20px;height:70px" src="data:image/png;base64,{{BARCODE2D::getBarcodePNG(route('detail', ['code' => $product->product_id]), 'QRCODE')}}"
 					alt="barcode" />
 				</h5>
-				<br>
-				<div id="test">
-					<a style="padding:10 15px; background:whitesmoke;margin-top:50px;" target="_blank" onclick="Website2APK.printPage()">Print</a>
-				</div>
+
+				<h5 style="text-align:center;:3px;font-size:15px;margin-top:-10px">{{ $product->product_serial_number }}</h5>
 
 			</div>
 		</div>
 	</div>
 </div>
+
+<script>
+	Website2APK.printPage();
+</script>
 
 <style>
 
