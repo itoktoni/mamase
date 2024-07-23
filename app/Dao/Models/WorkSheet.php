eb6a2d6294c4bf9d0dd3009c748c7529f3dae879
Itok toni laksono <itok.toni@gmail.com>
2024-07-23 09:21:28 +0700

    update mamase

--- app/Dao/Models/WorkSheet.php
+++ app/Dao/Models/WorkSheet.php
@@ -232,6 +232,8 @@ class WorkSheet extends Model
                 $model->{self::field_check_at()} = date('Y-m-d H:i:s');
             }
 
+            $model->{self::field_implementor()} = json_encode([strval(auth()->user()->id)]);
+
             if (request()->has('file_picture')) {
                 $file_logo = request()->file('file_picture');
                 $extension = $file_logo->getClientOriginalExtension();
