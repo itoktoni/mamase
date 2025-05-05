3a257296a69ce6aa932e672c9b53127047e32b52
Itok toni laksono <itok.toni@gmail.com>
2025-05-05 09:55:48 +0700

    update

--- app/Console/Commands/SendNotification.php
+++ app/Console/Commands/SendNotification.php
@@ -37,7 +37,6 @@ class SendNotification extends Command
         foreach ($data as $item)
         {
             $check = $notification->send($item->field_name, $item->field_phone, $item->field_description, $item->field_image);
-            dd($check);
             $item->notification_status = NotificationStatus::Sent;
             $item->notification_tanggal = date('Y-m-d');
 
